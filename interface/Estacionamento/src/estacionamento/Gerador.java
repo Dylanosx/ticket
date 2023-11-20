1012eac8c04f2c4a0d8dfdd42f6efffc8b8fd2d2
Dylanosx <dylanandrade05@gmail.com>
2023-11-20 20:00:22 -0300

    estacionamento teste

---
+++ interface/Estacionamento/src/estacionamento/Gerador.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package estacionamento;
+
+
+public class Gerador {
+   private static final String[] NOMES_CARROS = {"Celta", "Palio", "Renault", "Hyundai", "Ferrari"};
+
+    private static final String[] LETRAS_PLACA = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M",
+            "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"};
+
+    private static final String[] DIGITOS_PLACA = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"};
+    
+}
